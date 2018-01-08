3aabfd75ca57a1e17544b8196ed4aa8d35141e94
N Syrotiuk <ns@localhost>
2018-01-08 09:55:16 +0000

    updated mock design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 /* Pieces of data required
 
-- random word
-- body parts
-- guessed letters
-
+- random deck of cards
+- player's total
+- dealer's total
+- balance in £ sterling
 
 Our state is:
-- currentLetter
-- mysteryWord
-
+- deck
+- player's cards
+- dealer's cards
+- hand over
+- game over
+- dealer winner
+- balance
+- bet in progress
+- next card
 
 */
 
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import card0 from './cards/jc.gif';
+import c2 from  './cards/2c.gif';
 import { Grid, Col, Image } from 'react-bootstrap';
 import './App.css';
 
@@ -26,10 +32,32 @@ class Header extends Component {
   }
 }
 
-class RenderCards extends Component {
+class Status extends Component {
   render() {
     return (
-      <Image className='cards' src={this.props.value} />
+      <Col className='status'>{this.props.status}</Col>
+    );
+  }
+}
+
+class RenderDealerCards extends Component {
+  render() {
+    return (
+      <div>
+        <Col>Dealer:</Col>
+        <Image className='cards' src={this.props.value} />
+      </div>
+    );
+  }
+}
+
+class RenderPlayerCards extends Component {
+  render() {
+    return (
+      <div>
+        <Col>Player:</Col>
+        <Image className='cards' src={this.props.value} />
+      </div>
     );
   }
 }
@@ -59,11 +87,16 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      deck: [],
       nextCard: null,
-      dealerHand: [],
-      playerHand: [],
+      dealerHand: [c2],
+      playerHand: [c2],
       value: '', /* bet */
       balance: 500,
+      startHand: true,
+      handOver: false,
+      gameOver: false,
+      betInProgress: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -71,26 +104,50 @@ class Game extends Component {
   }
 
   handleChange(e) {
-    const dealerHand = this.state.dealerHand.slice();
-    const playerHand = this.state.playerHand.slice();
     this.setState({
-      value: e.target.value.toUpperCase(),
-      dealerHand: dealerHand,
-      playerHand: playerHand,
+      value: e.target.value,
     });
   }
 
   handleSubmit(e) {
-    console.log('number entered: ' + this.state.value);
+    console.log('Bet entered: ' + this.state.value);
     e.preventDefault();
+    const bet = this.state.value;
+    const balance = this.state.balance;
+    if (bet <= balance) {
+      const playerHand = this.state.playerHand.slice();
+      this.setState({
+        value: bet,
+        playerHand: playerHand,
+        betInProgress: false,
+        balance: balance - bet,
+      });
+    } else {
+        alert('Illegal bet: ' + bet + ' > ' + balance);
+    }
   }
 
   render() {
+//    const deck = shuffleDeck(this.state); 
+    const startHand = this.state.startHand;
+    if (startHand) {
+      dealFourCards(this.state);
+    }
+    const result = playHand(this.state);
+    let status;
+    if (result) {
+      status = result;
+    } else {
+        status = 'Status';
+    }
+
     return (
       <Grid className='grid'>
         <Header />
         <div className='card-panel'>
-          <RenderCards value={card0}/>
+          <RenderDealerCards value={this.state.dealerHand} />
+          <RenderPlayerCards value={this.state.playerHand} />
+          <Status status={status} />
         </div>
         <div className='money-panel'>
           <Balance money={this.state.balance} />
@@ -103,6 +160,8 @@ class Game extends Component {
               required
               value={this.state.value}
               onChange={this.handleChange}
+              disabled={this.state.betOver}
+              alt='Place your bet'
             />
           </form>
         </div>
@@ -119,5 +178,48 @@ class App extends Component {
     );
   }
 }
+
+function dealFourCards(state) {
+  //to do
+  const len = state.deck.length;
+  if (len <= 14) { //14 because unlikely for each player to have 7 cards
+    state.deck = shuffleDeck(state);
+  }
+  for (var i=1; i<5; i++) {
+    dealCard();
+  }
+}
+
+function dealCard(state) {
+
+
+
+}
+
+
+function playHand(state) {
+  //to do
+
+
+}
+
+
+function shuffleDeck(state) {
+  console.log('Shuffling deck...');
+  const clubs    = ['ac', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'tc', 'jc', 'qc', 'kc'];
+  const diamonds = ['ad', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'td', 'jd', 'qd', 'kd'];
+  const hearts   = ['ah', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'th', 'jh', 'qh', 'kh'];
+  const spades   = ['as', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'ts', 'js', 'qs', 'ks'];
+
+  var deck = state.deck;
+  deck = clubs.concat(diamonds);
+  deck = deck.concat(hearts);
+  deck = deck.concat(spades);
+  console.log(deck);
+  return deck;
+}
+
+
+
 
 export default App;
