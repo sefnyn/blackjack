12126a0b466689fa69ad49f575b7166edda6ad3d
N Syrotiuk <ns@localhost>
2018-01-09 13:23:33 +0000

    imported all cards and updated dealer and player render functions

--- src/App.js
+++ src/App.js
@@ -20,9 +20,68 @@ Our state is:
 
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import c2 from  './cards/2c.gif';
 import { Grid, Col, Image } from 'react-bootstrap';
 import './App.css';
+
+//clubs
+import c2 from  './cards/2c.gif';
+import c3 from  './cards/3c.gif';
+import c4 from  './cards/4c.gif';
+import c5 from  './cards/5c.gif';
+import c6 from  './cards/6c.gif';
+import c7 from  './cards/7c.gif';
+import c8 from  './cards/8c.gif';
+import c9 from  './cards/9c.gif';
+import tc from  './cards/tc.gif';
+import jc from  './cards/jc.gif';
+import qc from  './cards/qc.gif';
+import kc from  './cards/kc.gif';
+import ac from  './cards/ac.gif';
+
+//diamonds
+import d2 from  './cards/2d.gif';
+import d3 from  './cards/3d.gif';
+import d4 from  './cards/4d.gif';
+import d5 from  './cards/5d.gif';
+import d6 from  './cards/6d.gif';
+import d7 from  './cards/7d.gif';
+import d8 from  './cards/8d.gif';
+import d9 from  './cards/9d.gif';
+import td from  './cards/td.gif';
+import jd from  './cards/jd.gif';
+import qd from  './cards/qd.gif';
+import kd from  './cards/kd.gif';
+import ad from  './cards/ad.gif';
+
+//hearts
+import h2 from  './cards/2h.gif';
+import h3 from  './cards/3h.gif';
+import h4 from  './cards/4h.gif';
+import h5 from  './cards/5h.gif';
+import h6 from  './cards/6h.gif';
+import h7 from  './cards/7h.gif';
+import h8 from  './cards/8h.gif';
+import h9 from  './cards/9h.gif';
+import th from  './cards/th.gif';
+import jh from  './cards/jh.gif';
+import qh from  './cards/qh.gif';
+import kh from  './cards/kh.gif';
+import ah from  './cards/ah.gif';
+
+//spades
+import s2 from  './cards/2s.gif';
+import s3 from  './cards/3s.gif';
+import s4 from  './cards/4s.gif';
+import s5 from  './cards/5s.gif';
+import s6 from  './cards/6s.gif';
+import s7 from  './cards/7s.gif';
+import s8 from  './cards/8s.gif';
+import s9 from  './cards/9s.gif';
+import ts from  './cards/ts.gif';
+import js from  './cards/js.gif';
+import qs from  './cards/qs.gif';
+import ks from  './cards/ks.gif';
+import as from  './cards/as.gif';
 
 class Header extends Component {
   render() {
@@ -40,26 +99,30 @@ class Status extends Component {
   }
 }
 
-class RenderDealerCards extends Component {
-  render() {
-    return (
-      <div>
-        <Col>Dealer:</Col>
-        <Image className='cards' src={this.props.value} />
-      </div>
-    );
-  }
+function RenderDealerCards(props) {
+  const hand  = props.cards;
+  const cards = hand.map((card) =>
+    <Image key={card} className='cards' src={card} />
+  );
+  return (
+    <div>
+      <Col>Dealer:</Col>
+      {cards}
+    </div>
+  );
 }
 
-class RenderPlayerCards extends Component {
-  render() {
-    return (
-      <div>
-        <Col>Player:</Col>
-        <Image className='cards' src={this.props.value} />
-      </div>
-    );
-  }
+function RenderPlayerCards(props) {
+  const hand  = props.cards;
+  const cards = hand.map((card) =>
+    <Image key={card} className='cards' src={card} />
+  );
+  return (
+    <div>
+      <Col>Player:</Col>
+      {cards}
+    </div>
+  );
 }
 
 class Balance extends Component {
@@ -89,14 +152,14 @@ class Game extends Component {
     this.state = {
       deck: [],
       nextCard: null,
-      dealerHand: [c2],
-      playerHand: [c2],
+      dealerHand: [ac, c2, d2, h2, s2, c3],
+      playerHand: [as, td],
       value: '', /* bet */
       balance: 500,
       startHand: true,
       handOver: false,
       gameOver: false,
-      betInProgress: true,
+      betOver: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -119,7 +182,7 @@ class Game extends Component {
       this.setState({
         value: bet,
         playerHand: playerHand,
-        betInProgress: false,
+        betOver: true,
         balance: balance - bet,
       });
     } else {
@@ -145,8 +208,8 @@ class Game extends Component {
       <Grid className='grid'>
         <Header />
         <div className='card-panel'>
-          <RenderDealerCards value={this.state.dealerHand} />
-          <RenderPlayerCards value={this.state.playerHand} />
+          <RenderDealerCards cards={this.state.dealerHand} />
+          <RenderPlayerCards cards={this.state.playerHand} />
           <Status status={status} />
         </div>
         <div className='money-panel'>
@@ -206,20 +269,18 @@ function playHand(state) {
 
 function shuffleDeck(state) {
   console.log('Shuffling deck...');
-  const clubs    = ['ac', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'tc', 'jc', 'qc', 'kc'];
-  const diamonds = ['ad', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'td', 'jd', 'qd', 'kd'];
-  const hearts   = ['ah', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'th', 'jh', 'qh', 'kh'];
-  const spades   = ['as', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'ts', 'js', 'qs', 'ks'];
+  const clubs    = [c2, c3, c4, c5, c6, c7, c8, c9, tc, jc, qc, kc, ac];
+  const diamonds = [d2, d3, d4, d5, d6, d7, d8, d9, td, jd, qd, kd, ad];
+  const hearts   = [h2, h3, h4, h5, h6, h7, h8, h9, th, jh, qh, kh, ah];
+  const spades   = [s2, s3, s4, s5, s6, s7, s8, s9, ts, js, qs, ks, as];
 
   var deck = state.deck;
   deck = clubs.concat(diamonds);
   deck = deck.concat(hearts);
   deck = deck.concat(spades);
-  console.log(deck);
+//  console.log(deck);
   return deck;
 }
-
-
 
 
 export default App;
